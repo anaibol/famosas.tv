8ca7939cd2cf4459d5f74736d6f26c8395f4c9bf
anaibol <hey@anib.al>
2015-09-03 22:34:40 +0200

    New layout

--- app.js
+++ app.js
@@ -32,7 +32,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'bower_components')));
 
 app.use('/sitemap', sitemap);
 app.use('/', routes);
